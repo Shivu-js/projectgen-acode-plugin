aa46b1e0f0196d9540960ce2bc766c3ff1025f78
Shivu-js <sushil912031@gmail.com>
2026-02-08 23:36:07 +0530

    Create projects.js

---
+++ data/projects.js
@@ -0,0 +1,14 @@
+export default [
+  {
+    title: "Todo List App",
+    level: "beginner",
+    tech: ["html","css","js"],
+    description: "Simple todo list app for beginners.",
+    structure: ["index.html","style.css","script.js"],
+    starter:{
+      html:"<!DOCTYPE html><html><body><h1>Todo</h1></body></html>",
+      css:"body{background:#111;color:#fff}",
+      js:"// js here"
+    }
+  }
+];
